02b485f6feb195fe5f29a9f3b1734cf708e96449
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 04:39:45 +0800

    pseudocode

---
+++ greater_number_between_two_numbers.py
@@ -0,0 +1,24 @@
+# Inducil, Raphael CLouiee A.
+# 3-12-06
+# batch 1, Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# send message instruction to user
+# ask user to input first number
+# ask user to input second number
+
+print("Greater number between two numbers")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# compare the two numbers
+# print the bigger number
+
+if num1 > num2:
+    print("The greater number is: ", num1)
+elif num1 < num2:
+    print("The greater number is: ", num2)
+else:
+    print("The numbers and are equal")
+
+# end
