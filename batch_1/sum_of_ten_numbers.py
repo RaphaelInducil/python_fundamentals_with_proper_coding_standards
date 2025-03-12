d88a95a744cb454594e2b26a5dcf5679d2e2a7fc
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 06:36:28 +0800

    pseudocode

---
+++ batch_1/sum_of_ten_numbers.py
@@ -0,0 +1,7 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# send message instruction to user
+# range(10) ask for input, sum it up
+# display output
