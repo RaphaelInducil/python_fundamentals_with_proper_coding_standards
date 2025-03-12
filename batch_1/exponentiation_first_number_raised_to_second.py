2a77861ad29796a64f7dd5f4da50f304a2f84b93
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 06:25:44 +0800

    back end, n1^n2 calculator. code complete

--- batch_1/exponentiation_first_number_raised_to_second.py
+++ batch_1/exponentiation_first_number_raised_to_second.py
@@ -11,5 +11,8 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # make num1 ^ num2
-# print the result of the first number raised to the second number
+
+raised = num1 ** num2
+print("The result when the first number is raised to the second number is: ", raised)
+
 # end
