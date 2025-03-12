94d080716a75654d9e6061b3695a9d793041537d
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 06:14:15 +0800

    front end, ask user for input

--- batch_1/quotient_of_two_numbers_decimal_format.py
+++ batch_1/quotient_of_two_numbers_decimal_format.py
@@ -5,6 +5,11 @@
 # send message instruction to user
 # ask user to input first number
 # ask user to input second number
+
+print("5. Quotient of two numbers")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # divide the two numbers
 # print the quotient of the two numbers
 # end
