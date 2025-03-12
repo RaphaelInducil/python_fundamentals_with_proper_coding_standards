ca30701fe24bd43d9be7f537f7a75d7104e55e70
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 05:36:29 +0800

    pseudocode

---
+++ batch_1/sum_of_two_numbers.py
@@ -0,0 +1,10 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+# send message instruction to user
+# ask user to input first number
+# ask user to input second number
+#add the two numbers
+# print the sum of the two numbers
+#end
