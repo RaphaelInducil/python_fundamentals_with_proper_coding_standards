1eff4e097a5b4d1cba062186fb796c5d16ea5fea
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 04:28:44 +0800

    pseudocode

---
+++ batch_1/check_if_two_numbers_are_equal.py
@@ -0,0 +1,10 @@
+# Inducil, Raphael CLouiee A.
+# 3-12-06
+# batch 1, Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# send message instruction to user
+# ask user to input first number
+# ask user to input second number
+# condition to check if the two numbers are equal
+# print "Equal" if the two numbers are equal
+# end
