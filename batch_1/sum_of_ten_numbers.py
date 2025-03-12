06bf620cfe8b6804253be25f8c1c7a91387a2f7c
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 06:38:15 +0800

    display title, 10 number sum calculator

--- batch_1/sum_of_ten_numbers.py
+++ batch_1/sum_of_ten_numbers.py
@@ -3,5 +3,11 @@
 # batch 1, Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # send message instruction to user
+
+print("7. Sum of Ten Numbers")
+
 # range(10) ask for input, sum it up
+
+total_sum = sum(int(input(f"Enter number {i + 1}: ")) for i in range(10))
+
 # display output
