2bbf27a6c205504df37b253074b7301914c9672d
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 05:44:51 +0800

    pseudocode

---
+++ batch_1/product_of_two_numbers.py
@@ -0,0 +1,10 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# send message instruction to user
+# ask user to input first number
+# ask user to input second number
+# multiply the two numbers
+# print the product of the two numbers
+# end
