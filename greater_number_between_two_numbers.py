5fe350d9a03e0795bb6061a51757b5769f1e7014
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 04:06:57 +0800

    forgot to save the pseudocode

--- greater_number_between_two_numbers.py
+++ greater_number_between_two_numbers.py
@@ -1,4 +1,10 @@
 # Inducil, Raphael CLouiee A.
 # 3-12-06
-# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-# making it with
+# batch 1, Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# send message instruction to user
+# ask user to input first number
+# ask user to input second number
+# compare the two numbers
+# print the bigger number
+# end
