981d06f1720c82a7a557494a39ac0b03bde7e0f2
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 06:42:55 +0800

    pseudocode

---
+++ batch_1/count_of_odd_numbers_among_ten_inputs.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# send message instruction to user
+# range(10), / with 2, if remainder is not 0, count it
+# print the count of odd numbers
+# end
