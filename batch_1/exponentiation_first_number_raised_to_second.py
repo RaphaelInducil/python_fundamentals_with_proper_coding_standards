4001366de3d4e74be8c8295860da12abe84b3568
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 06:21:48 +0800

    pseudocode

---
+++ batch_1/exponentiation_first_number_raised_to_second.py
@@ -0,0 +1,10 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+# send message instruction to user
+# ask user to input first number
+# ask user to input second number
+# make num1 ^ num2
+# print the result of the first number raised to the second number
+# end
