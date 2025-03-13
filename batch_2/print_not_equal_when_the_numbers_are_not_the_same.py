d83ffaae62084360d7b8f42b09ae7c626c96d6c0
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 21:49:31 +0800

    pseudocode

---
+++ batch_2/print_not_equal_when_the_numbers_are_not_the_same.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-12-25
+# batch 2, Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+# user input 2 numbers
+# compare 2 numbers
+# print "not equal" if not same number
+# end
