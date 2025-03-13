f6ac02f46e1a1cdf517f32b2a3bbb339ce59cfc3
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 23:17:18 +0800

    pseudocode

---
+++ batch_2/print_all_the_numbers_between_the_two_numbers.py
@@ -0,0 +1,7 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask user to input 2 numbers
+# for range num1 to num2, print num1 + 1 until num2
+# end
