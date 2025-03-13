8b65e7892471cd6b27cd543648d8074387645e3f
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:17:48 +0800

    pseudocode

---
+++ batch_2/print_the_difference_of_the_two_numbers.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-12-25
+# batch 2, Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# user input 2 numbers
+# minus num1 to num2
+# print the difference
+# end
