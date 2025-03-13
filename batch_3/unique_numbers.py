49e561a598c0064f62c249c2c16759ea552e0465
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 00:04:44 +0800

    pseudocode

---
+++ batch_3/unique_numbers.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 3, Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# user input 10 num
+# create a list for num
+# limit duplicate using numbers.count
+# print numbers only once
+# end
