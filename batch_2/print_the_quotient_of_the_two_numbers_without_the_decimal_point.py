2961b06b6e56d43bc78fcef102393eb2e25daf42
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:37:39 +0800

    pseudocode

---
+++ batch_2/print_the_quotient_of_the_two_numbers_without_the_decimal_point.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+# user input 2 numbers
+# divide the numbers
+# print the quotient(int)
+# end
