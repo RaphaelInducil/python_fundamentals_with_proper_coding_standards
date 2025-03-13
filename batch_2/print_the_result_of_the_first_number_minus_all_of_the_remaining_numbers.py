91cf2c32eb01c43d3d48b35634c8ed011679e1ae
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:48:46 +0800

    pseudocode

---
+++ batch_2/print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all the remaining numbers.
+
+# user input  range 10 numbers
+# num1 - sum of num2 to num10
+# print the result
+# end
