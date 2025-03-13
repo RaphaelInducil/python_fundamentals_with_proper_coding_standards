7efd50c386582fb259eb6247a279fb89b8bcb2c6
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 13:59:47 +0800

    pseudocode

---
+++ batch_1/numbers_from_0_to_100_except_those_ending_in_zero.py
@@ -0,0 +1,7 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# send message instruction to user
+# for loop, range 101, if remainder is not 0, print output
+# end
