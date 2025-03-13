f348b769e0e04bf1633493e7b9088910ea61684b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 23:09:58 +0800

    pseudocode

---
+++ batch_2/print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_five.py
@@ -0,0 +1,7 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# while loop 100, print num, add 1 to num
+# if remainder is not 0 or 5, print output
+# end
