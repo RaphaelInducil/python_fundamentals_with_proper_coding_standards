ae8b0486623f38f1288ec0004c205bc4a33692de
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 23:06:47 +0800

    pseudocode typo removed, while loop to 100 with condition of <= 100 and += 2, print num

--- batch_2/print_all_the_odd_numbers_starting_from_0_to_100.py
+++ batch_2/print_all_the_odd_numbers_starting_from_0_to_100.py
@@ -2,8 +2,10 @@
 # 3-13-25
 # batch 2, Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-# user input 10 numbers
-# while loop 100
-# print num
-# add 2 to num
+# while loop 100, print num, add 2 to num
+
+while num <= 100:
+    print(num)
+    num += 2
+
 # end
