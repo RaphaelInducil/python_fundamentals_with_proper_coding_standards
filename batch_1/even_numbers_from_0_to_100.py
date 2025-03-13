0bc98fa5726d836dda90050a8c4e33fdf5762b67
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 13:51:47 +0800

    pseudocode

---
+++ batch_1/even_numbers_from_0_to_100.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-06
+# batch 1, Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+# send message instruction to user
+# print all the even numbers starting from 0 to 100
+# use for loop
+# end
