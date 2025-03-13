a8a07c87ad6c50567ec17bd742715337a087ffed
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 23:04:06 +0800

    pseudocode

---
+++ batch_2/print_all_the_odd_numbers_starting_from_0_to_100.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+# user input 10 numbers
+# while loop 100
+# print num
+# add 2 to num
+# end
