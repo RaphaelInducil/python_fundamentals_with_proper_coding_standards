7168b20c38c8a19a7b7bfb174588d0cac02de634
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 21:45:02 +0800

    rename values, if-else condition for smaller number. code complete

--- batch_2/the_smaller_number.py
+++ batch_2/the_smaller_number.py
@@ -1,12 +1,18 @@
 # Inducil, Raphael CLouiee A.
-# 3-12-06
+# 3-12-25
 # batch 2, Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # user input 2 numbers
 
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
 
 # check which number is smaller
 # print the smaller number
+
+if num1 < num2:
+    print(num1, "is smaller")
+else:
+    print(num2, "is smaller")
+
 # end
