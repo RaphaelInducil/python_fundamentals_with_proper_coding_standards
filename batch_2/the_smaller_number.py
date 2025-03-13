5810ae780ce132c6074c885952595721dd9c7e19
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 21:42:42 +0800

    ask user input

--- batch_2/the_smaller_number.py
+++ batch_2/the_smaller_number.py
@@ -2,8 +2,11 @@
 # 3-12-06
 # batch 2, Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
-# print program title
 # user input 2 numbers
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
 # check which number is smaller
 # print the smaller number
 # end
