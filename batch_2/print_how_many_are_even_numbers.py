17e06056f7bdbdf6088bde58befe8fcf91bbeea6
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:58:19 +0800

    ask for user input

--- batch_2/print_how_many_are_even_numbers.py
+++ batch_2/print_how_many_are_even_numbers.py
@@ -3,6 +3,10 @@
 # batch 2, # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
 # user input 10 numbers
+
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+
 # check if the number is even
 # count the even numbers
 # print the count of even numbers
