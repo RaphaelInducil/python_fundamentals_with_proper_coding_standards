1b5b8a428869b3e093b409592a2daa05205a2998
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:56:58 +0800

    pseudocode

---
+++ batch_2/print_how_many_are_even_numbers.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# user input 10 numbers
+# check if the number is even
+# count the even numbers
+# print the count of even numbers
+# end
