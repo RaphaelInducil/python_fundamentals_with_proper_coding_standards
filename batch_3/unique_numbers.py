9e6b61c66bdacf67b1db37c641aa100354892e51
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 00:16:24 +0800

    add new value, num loop in through numbers, limit duplicate using .count == 1, display list of unique numbers. code complete

--- batch_3/unique_numbers.py
+++ batch_3/unique_numbers.py
@@ -2,15 +2,19 @@
 # 3-13-25
 # batch 3, Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
+# create a list for num
+
+numbers = []
+
 # user input 10 num
 
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
-
-# create a list for num
-
     numbers.append(num)
 
-# limit duplicate using numbers.count
-# print numbers only once
+# limit duplicate using numbers.count == 1, print unique numbers
+
+unique_numbers = [num for num in numbers if numbers.count(num) == 1]
+print("Unique numbers:", unique_numbers)
+
 # end
