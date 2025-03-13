d24735566cd3e787f1c314c942ef260c1273675a
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:44:24 +0800

    pseudocode

---
+++ batch_2/print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-13-25
+# batch 2, Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+# user input 2 numbers
+# modulus number1 to number2
+# print the remainder
+# end
