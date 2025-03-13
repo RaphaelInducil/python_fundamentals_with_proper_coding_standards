91c5a2a5f8ddff45e7c613e63a90c40663384dc2
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 23:02:34 +0800

    description typo. code complete

--- batch_2/print_how_many_are_even_numbers.py
+++ batch_2/print_how_many_are_even_numbers.py
@@ -1,6 +1,6 @@
 # Inducil, Raphael CLouiee A.
 # 3-13-25
-# batch 2, # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+# batch 2, Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
 # user input 10 numbers
 
