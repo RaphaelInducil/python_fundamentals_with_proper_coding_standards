e1799178d80b46813513c3ebd4264af92196dad6
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 22:51:15 +0800

    ask user to input 10 nums, append inputted numbers

--- batch_2/print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ batch_2/print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -4,5 +4,11 @@
 
 # user input  range 10 numbers
 # num1 - sum of num2 to num10
+
+numbers = []
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+
 # print the result
 # end
