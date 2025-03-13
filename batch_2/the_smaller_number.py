d720a9059f13b2262b1f5a2a5925fe8049bc655b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-13 21:38:27 +0800

    pseudocode

---
+++ batch_2/the_smaller_number.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael CLouiee A.
+# 3-12-06
+# batch 2, Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# print program title
+# user input 2 numbers
+# check which number is smaller
+# print the smaller number
+# end
