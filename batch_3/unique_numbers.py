bd191fd21642420f6ba19338954a77ad462eed8c
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 00:06:49 +0800

    user input, and append list

--- batch_3/unique_numbers.py
+++ batch_3/unique_numbers.py
@@ -3,7 +3,14 @@
 # batch 3, Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # user input 10 num
+
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+
 # create a list for num
+
+    numbers.append(num)
+
 # limit duplicate using numbers.count
 # print numbers only once
 # end
