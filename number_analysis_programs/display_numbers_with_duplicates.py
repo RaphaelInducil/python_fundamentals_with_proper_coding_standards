2465f348d9ddfdf02dfb593f2848937d6652e90b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:57:50 +0800

    initialize numbers, range user input to 10, append numbers, added duplicates as new value

--- number_analysis_programs/display_numbers_with_duplicates.py
+++ number_analysis_programs/display_numbers_with_duplicates.py
@@ -3,9 +3,20 @@
 # batch 4, Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # initialize list for numbers
+
+numbers = []
+
 # for i in range, input 10 numbers
 # append numbers to list
-# add duplicate as new value
+
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+
+# create duplicate as new value
+
+duplicates = set()
+
 # if num > 1 count the numbers, add to duplicate
 # print duplicate list
 # end
