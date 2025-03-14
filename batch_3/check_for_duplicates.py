a308da40fe4635cd8994d98f537bed4e09baa187
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:08:09 +0800

    initialize value, while loop, user input, conditional sorting of duplicate and unique in a list

--- batch_3/check_for_duplicates.py
+++ batch_3/check_for_duplicates.py
@@ -3,9 +3,23 @@
 # batch 3, Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 # initialize list for numbers
+
+numbers = []
+
 # while loop, continue until user input is invalid
 # input number
+
+while True:
+    num = int(input("Enter a number: "))
+
 # if num is in numbers print duplicates
 # else append num to numbers print unique
+
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.append(num)
+
 # value error for invalid input
 # end
