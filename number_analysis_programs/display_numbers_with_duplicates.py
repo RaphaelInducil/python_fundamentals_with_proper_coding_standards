92f45b3d25b9110fd800248435618364578eca5a
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:55:39 +0800

    pseudocode

---
+++ number_analysis_programs/display_numbers_with_duplicates.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 4, Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# initialize list for numbers
+# for i in range, input 10 numbers
+# append numbers to list
+# add duplicate as new value
+# if num > 1 count the numbers, add to duplicate
+# print duplicate list
+# end
