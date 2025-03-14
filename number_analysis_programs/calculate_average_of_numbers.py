217f6f562abbaaa52292746e26e1ee8f30caca19
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 16:05:39 +0800

    pseudocode

---
+++ number_analysis_programs/calculate_average_of_numbers.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 4, Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# initialize numbers list
+# while loop,user input
+# append numbers
+# error handling
+# sum numbers to get average // number for input
+# print average
+# end
