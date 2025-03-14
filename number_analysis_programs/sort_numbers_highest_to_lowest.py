674f7bcda0f1e70e6446cf3e2ca706a7408655df
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 15:43:24 +0800

    initialize numbers as list, while loop, user input, append num, error handling for invalid code

--- number_analysis_programs/sort_numbers_highest_to_lowest.py
+++ number_analysis_programs/sort_numbers_highest_to_lowest.py
@@ -3,9 +3,20 @@
 # batch 4, Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
 # initialize numbers as list value
+
+numbers = []
+
 # while loop, ask user input
 # append num
 # error handling
+
+while True:
+    try:
+        num = int(input("Enter a number (or any non-number to stop): "))
+        numbers.append(num)
+    except ValueError:
+        break
+
 # sort numbers
 # print sorted numbers
 # end
