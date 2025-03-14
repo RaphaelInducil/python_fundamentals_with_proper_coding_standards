9973334edd8079eaecb6dc2428673a484b40eefd
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 15:20:37 +0800

    pseudocode

---
+++ number_analysis_programs/display_most_frequent_number.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 4, Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# initialize numbers
+# while loop, user input
+# if num in numbers count the nummbers
+# else dont count
+# value error for invalid input, break
+# print the number with most duplicate
+# end
