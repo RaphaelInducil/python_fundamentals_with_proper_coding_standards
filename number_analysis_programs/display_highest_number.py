ec2a2f8c8d6f851c1748254791ad06a9c4150d88
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 15:31:44 +0800

    pseudocode

---
+++ number_analysis_programs/display_highest_number.py
@@ -0,0 +1,10 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 4, Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+# initialize highest as value
+# while loop, ask user input
+# if num > highest, turn highest into num
+# error handling for invalid input
+# print highest
+# end
