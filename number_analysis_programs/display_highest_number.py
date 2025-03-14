f3b9dac63ffca393d47930fd78897bb7a2f19d17
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 15:36:44 +0800

    initialize highest as value, while loop, user input, if num is greater change highest to num

--- number_analysis_programs/display_highest_number.py
+++ number_analysis_programs/display_highest_number.py
@@ -3,8 +3,17 @@
 # batch 4, Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 # initialize highest as value
+
+highest = None
+
 # while loop, ask user input
 # if num > highest, turn highest into num
+
+while True:
+        num = int(input("Enter a number (or any non-number to stop): "))
+        if highest is None or num > highest:
+            highest = num
+
 # error handling for invalid input
 # print highest
 # end
