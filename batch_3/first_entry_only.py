e9845dcdc90311b1eee432877b7a124204b4bb66
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 13:43:26 +0800

    pseudocode

---
+++ batch_3/first_entry_only.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 3, Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# user input 10 numbers
+# append list numbers
+# if num is not unique, append only the unique numbers
+# print unique numbers
