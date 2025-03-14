b1c49baa6ab1cc846140e103bccac63906361302
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 16:06:47 +0800

    initialize numbers list, while loop user input, append numbers, error handling

--- number_analysis_programs/calculate_average_of_numbers.py
+++ number_analysis_programs/calculate_average_of_numbers.py
@@ -3,9 +3,20 @@
 # batch 4, Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 # initialize numbers list
+
+numbers = []
+
 # while loop,user input
 # append numbers
 # error handling
+
+while True:
+    try:
+        num = int(input("Enter a number (or any non-number to stop): "))
+        numbers.append(num)
+    except ValueError:
+        break
+
 # sum numbers to get average // number for input
 # print average
 # end
