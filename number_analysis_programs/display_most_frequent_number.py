9724124fa11d5861ee0939c50bcfc36eb60e092e
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 15:24:46 +0800

    initialize numbers, while loop, user input, count duplicate

--- number_analysis_programs/display_most_frequent_number.py
+++ number_analysis_programs/display_most_frequent_number.py
@@ -3,8 +3,17 @@
 # batch 4, Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 # initialize numbers
+
+numbers = {}
+
 # while loop, user input
 # if num in numbers count the nummbers
+
+while True:
+        num = int(input("Enter a number (or any non-numeric value to stop): "))
+        if num in numbers:
+            numbers[num] += 1
+
 # else dont count
 # value error for invalid input, break
 # print the number with most duplicate
