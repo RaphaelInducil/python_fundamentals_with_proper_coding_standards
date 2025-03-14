ced11f97948872256a47079df2b7845e55ceccb9
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:21:04 +0800

    initialize number holder (lowest), while loop for user input, conditional to change num to lowest

--- batch_3/lowest_number_loop.py
+++ batch_3/lowest_number_loop.py
@@ -3,10 +3,21 @@
 # batch 3, Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 # number holder (lowest)
+
+lowest = None
+
 # while loop
 # user input
+
+while true:
+    num = int(input("Enter a number: "))
+
 # if num < lowest
 # turn lowest into num
+
+    if lowest is None or num < lowest:
+        lowest = num
+
 # error handling for invalid input
 # print lowest
 # end
