ac1bd478c6784bc2ad566ada2969e362a79ebd6b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:05:36 +0800

    pseudocode

---
+++ batch_3/check_for_duplicates.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 3, Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+# initialize list for numbers
+# while loop, continue until user input is invalid
+# input number
+# if num is in numbers print duplicates
+# else append num to numbers print unique
+# value error for invalid input
+# end
