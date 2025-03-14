cc95a6fa72daee7cd71475a8d35aa0a7d799c6d2
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 15:41:33 +0800

    pseudocode

---
+++ number_analysis_programs/sort_numbers_highest_to_lowest.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 4, Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+# initialize numbers as list value
+# while loop, ask user input
+# append num
+# error handling
+# sort numbers
+# print sorted numbers
+# end
