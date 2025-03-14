e7efebf7bec6a8c87ea6ad4d6149992ed66b9551
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:28:27 +0800

    pseudocode

---
+++ batch_3/ascending_numbers_loop.py
@@ -0,0 +1,11 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 3, Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+# initialize list holder
+# while loop, ask user input
+# append input value to list holder
+# sort list holder
+# print sorted list holder
+# error handling for invalid input
+# end
