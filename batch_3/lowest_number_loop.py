3a0768ad155c52850895721490e43bd7ddccc6e8
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:19:03 +0800

    pseudocode

---
+++ batch_3/lowest_number_loop.py
@@ -0,0 +1,12 @@
+# Inducil, Raphael CLouiee A.
+# 3-14-25
+# batch 3, Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# number holder (lowest)
+# while loop
+# user input
+# if num < lowest
+# turn lowest into num
+# error handling for invalid input
+# print lowest
+# end
