6186b0503a313e31722a6529e9cac2e19d2e7c51
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-14 14:30:30 +0800

    initialize list holder, while loop, user input, number sorter, display output

--- batch_3/ascending_numbers_loop.py
+++ batch_3/ascending_numbers_loop.py
@@ -3,9 +3,21 @@
 # batch 3, Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
 # initialize list holder
+
+numbers = []
+
 # while loop, ask user input
 # append input value to list holder
+
+while True:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+
 # sort list holder
 # print sorted list holder
+
+        numbers.sort()
+        print("Numbers from lowest to highest:", numbers)
+
 # error handling for invalid input
 # end
